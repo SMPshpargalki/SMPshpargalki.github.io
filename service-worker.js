2db69d3ede61b083d496d242186ad1fb7c4a3a1a
SMPshpargalki <kirill1320101991@gmail.com>
2025-11-13 20:33:35 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,1 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("smp-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/styles.css", "/manifest.json", "/icon.png"]);
-    })
-  );
-});
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
