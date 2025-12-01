75470ac6cd4a38563fbb4d72a28eb678d7e95821
SMPshpargalki <kirill1320101991@gmail.com>
2025-12-01 15:50:17 +0300

    Create Анестезиология.js

---
+++ Шаблоны/Анестезиология.js
@@ -0,0 +1,36 @@
+// Создаем массив кнопок и текста
+const АнестезиологияTemplates = [
+    
+    { text: "Анафилактический шок", modal: `текст` },
+    
+    { text: "Клиническая смерть до СМП", modal: `текст` },
+    
+    { text: "Клиническая смерть. Повешение", modal: `текст` },
+    
+    { text: "Кома неясной этиологии", modal: `текст` },
+    
+    { text: "Кома. Сахарный диабет", modal: `текст` },
+    
+    { text: "Кома. Смерть при бригаде", modal: `текст` },
+    
+    { text: "Отек мозга", modal: `текст` }    
+];
+
+
+// Создаем кнопки после загрузки DOM
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("Анестезиология");
+
+    if (!container) {
+        console.error("Container #Анестезиология не найден.");
+        return;
+    }
+
+    АнестезиологияTemplates.forEach(item => {
+        const btn = document.createElement("button");
+        btn.className = "child-btn";
+        btn.textContent = item.text;
+        btn.onclick = () => openModal(item.modal);
+        container.appendChild(btn);
+    });
+});
