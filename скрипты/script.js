81ed927ae2e1d80d10d3ce8aa31f2c74584f4d89
SMPshpargalki <kirill1320101991@gmail.com>
2025-12-15 20:31:53 +0300

    Update script.js

--- скрипты/script.js
+++ скрипты/script.js
@@ -292,24 +292,3 @@ document.getElementById("calcPreg").addEventListener("click", function () {
     document.getElementById("pregDue").innerText = "Предполагаемая дата родов: " + formatDate(due);
     document.getElementById("pregResult").style.display = "block";
 });
-
-function updateNEWS() {
-  let total = 0;
-
-  document.querySelectorAll("[data-score]").forEach(select => {
-    const score = Number(select.value || 0);
-    total += score;
-
-    const scoreSpan = document.getElementById(select.id + "Score");
-    if (scoreSpan) scoreSpan.textContent = score;
-  });
-
-  document.getElementById("newsTotal").textContent =
-    "Сумма баллов: " + total;
-}
-
-document.addEventListener("change", e => {
-  if (e.target.hasAttribute("data-score")) {
-    updateNEWS();
-  }
-});
